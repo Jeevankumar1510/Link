ae98164a3bdf2ac38a1cf3a2432ba792135a989c
JiyyaniJeevanKumar1510 <jeevana0508@gmail.com>
2026-06-25 07:49:34 +0530

    file creates

---
+++ app.py
@@ -0,0 +1,116 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+from sqlalchemy import text
+
+from database import engine
+
+import random
+import string
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Templates
+templates = Jinja2Templates(directory="templates")
+
+# Static Files
+
+
+def generate_code():
+    return ''.join(
+        random.choices(
+            string.ascii_letters + string.digits,
+            k=6
+        )
+    )
+
+@app.get("/")
+def home(request: Request):
+
+    with engine.connect() as conn:
+        urls = conn.execute(
+            text("""
+                SELECT *
+                FROM urls
+                ORDER BY id DESC
+            """)
+        ).fetchall()
+
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={
+            "request": request,
+            "urls": urls,
+            "base_url": str(request.base_url)
+        }
+    )
+
+
+@app.post("/shorten")
+async def shorten(request: Request):
+
+    form = await request.form()
+    original_url = form.get("url")
+
+    with engine.begin() as conn:
+
+        existing = conn.execute(
+            text("""
+                SELECT short_code
+                FROM urls
+                WHERE original_url=:url
+            """),
+            {"url": original_url}
+        ).fetchone()
+
+        if existing:
+
+            short_code = existing[0]
+
+        else:
+
+            short_code = generate_code()
+
+            conn.execute(
+                text("""
+                    INSERT INTO urls
+                    (original_url, short_code)
+                    VALUES
+                    (:url,:code)
+                """),
+                {
+                    "url": original_url,
+                    "code": short_code
+                }
+            )
+
+    return RedirectResponse("/", status_code=303)
+
+
+@app.get("/{code}")
+def redirect_url(code: str):
+
+    with engine.connect() as conn:
+        result = conn.execute(
+            text(
+                """
+                SELECT original_url
+                FROM urls
+                WHERE short_code = :code
+                """
+            ),
+            {
+                "code": code
+            }
+        ).fetchone()
+
+    if result:
+        return RedirectResponse(url=result[0])
+
+    return {
+        "error": "URL not found"
+    }
